97ee65f9982fc60f7ce9b083deb3d81065817954
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-03-10 19:55:37 +0300

    Add project files.

---
+++ Models/GameModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GameIndustry2.Models
+{
+    public class GameModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Image { get; set; }
+    }
+}
